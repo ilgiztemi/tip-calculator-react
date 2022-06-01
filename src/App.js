b9c0a9e49162d8d14f1f98eaea476b9df35643a5
Ilgiz Temi <temirbekov91@gmail.com>
2022-06-01 19:40:46 -0400

    tip calculator react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [bill, setBill] = useState("");
+  const [tips, setTips] = useState("");
+  const [person, setPerson] = useState("");
+  const [result, setResult] = useState("");
+  const handleOptions = (e) => {
+    setTips(e.target.value);
+  };
+  const handleCalculator = () => {
+    let res = (bill * tips) / person;
+    setResult(res);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>TIP CALCULATOR</h1>
+      <div className="bill">
+        <p>How much was your bill?</p>
+        <label>$</label>
+        <input
+          onChange={(e) => {
+            const input = e.target.value;
+            setBill(input);
+          }}
+          type="text"
+        />
+      </div>
+      <div className="service">
+        <p>How was your service?</p>
+        <select name="" id="select" onChange={(e) => handleOptions(e)}>
+          <option value="default" disabled>
+            --Choose an Option--
+          </option>
+          <option value="0.3">30% - Outstanding</option>
+          <option value="0.2">20% - Good</option>
+          <option value="0.15">15% - It was OK</option>
+          <option value="0.1">10% - Bad</option>
+          <option value="0.05">5% - Terrible</option>
+        </select>
+      </div>
+      <div className="people">
+        <p>How many people are sharing the bill?</p>
+        <input
+          onChange={(e) => {
+            const people = e.target.value;
+            setPerson(people);
+          }}
+          type="text"
+        />
+        <label>people</label>
+      </div>
+      <button onClick={handleCalculator} className="btn">CALCULATE!</button>
+      <div className="output">
+        {result && (
+          <>
+            <p>TIP AMOUNT</p>
+            <p>$ {result}</p>
+            <p>each</p>
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
